b1121e27f86b0e7e70641af1fc85a467d66410f9
Febry Nugroho <febryngrho@gmail.com>
2022-06-26 14:09:37 +0700

    challenge 09

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact'
+import Header from './components/Header'
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='master-card'>
+        {
+          contacts.map((item) => <Contact data={item} />)
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
